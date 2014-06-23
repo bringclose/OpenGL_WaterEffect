dda32d3183db8f45a1b5c9870a646cd903c96970
bringclose <spam1703@gmail.com>
2014-06-23 17:03:21 +0700

    + Add normal mapping for rock.

--- Resources/Shaders/NormalMapping.fs
+++ Resources/Shaders/NormalMapping.fs
@@ -2,12 +2,26 @@ precision mediump float;
 
 uniform sampler2D u_texture0;	//Rock
 uniform sampler2D u_texture1;	//RockNormalMap
+uniform vec3 u_lightPos;
+uniform vec4 u_lightColor;
 
+varying vec3 v_posW;
 varying vec2 v_uv;
+varying vec3 v_tangentW;
+varying vec3 v_bitangentW;
+varying vec3 v_normalW;
 
 void main()
 {
-	//gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-	gl_FragColor = texture2D(u_texture0, v_uv);
-	//gl_FragColor = texture2D(u_texture1, v_uv);
+	vec3 normal = texture2D(u_texture1, v_uv).xyz;
+    mat3 TBN = mat3(normalize(v_tangentW), normalize(v_bitangentW), normalize(v_normalW));
+    vec3 normalW = normalize(TBN * (2.0 * normal - 1.0));
+    vec3 lightDirection = normalize(v_posW - u_lightPos);
+    float diffuse = max(0.0, dot(lightDirection, normalW));
+    vec3 rockTexture = texture2D(u_texture0, v_uv).xyz;
+    //float diffuse = max(dot(normalW, lightDirection), 0.0);
+    //vec4 colorWater = vec4((diffuse * (colorReflection + u_lightColor)).rgb, colorReflection.a); 
+    vec4 colorRock = vec4(diffuse * rockTexture.rgb * u_lightColor.rgb, 1.0);
+    
+    gl_FragColor = colorRock;
 }
--- Resources/Shaders/NormalMapping.vs
+++ Resources/Shaders/NormalMapping.vs
@@ -1,14 +1,26 @@
 uniform mat4 u_mvp;
+uniform mat4 u_maWorld;
 
 attribute vec3 a_posL;
+attribute vec3 a_normalL;
+attribute vec3 a_tangentL;
+attribute vec3 a_bitangentL;
 attribute vec2 a_uv;
 
 varying vec2 v_uv;
+varying vec3 v_posW;
+varying vec3 v_tangentW;
+varying vec3 v_bitangentW;
+varying vec3 v_normalW;
 
 void main()
 {
 	vec4 posL = vec4(a_posL, 1.0);
 	gl_Position = u_mvp*posL;
 	
+	v_posW = (u_maWorld * posL).xyz;
+	v_normalW = normalize((u_maWorld * vec4(a_normalL, 0.0)).xyz);
+	v_tangentW = normalize((u_maWorld * vec4(a_tangentL, 0.0)).xyz);
+	v_bitangentW = normalize((u_maWorld * vec4(a_bitangentL, 0.0)).xyz);
 	v_uv = a_uv;
 }
