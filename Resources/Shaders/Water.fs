57b6eaec9a54beb362e77f7185af4c95798277af
bringclose <spam1703@gmail.com>
2014-06-24 18:30:52 +0700

    + Add height map for near far water eff.

--- Resources/Shaders/Water.fs
+++ Resources/Shaders/Water.fs
@@ -6,6 +6,8 @@ uniform mat4 u_maWorld;
 uniform sampler2D u_texture0;//rock
 uniform sampler2D u_texture1;//displacement
 uniform sampler2D u_texture2;//waternormal
+uniform sampler2D u_texture3;//HeightMap
+
 uniform samplerCube u_textureCube;
 uniform vec3 u_lightPos;
 uniform vec4 u_lightColor;
@@ -27,7 +29,7 @@ void main()
 	
 	//Displacement
 	vec2 disp = texture2D(u_texture1, vec2(v_uv.x, v_uv.y + u_time)).rg;
-	vec2 offset = (2.0 * disp - 1.0) * u_dMax;
+	vec2 offset = (4.0 * disp - 0.5) * u_dMax;
 	vec2 Tex_coords_displaced = v_uv + offset;
 	
 	//Reflection
@@ -36,7 +38,7 @@ void main()
     vec4 colorReflection = textureCube(u_textureCube,reflectDir);
     
     //Normal mapping
-    vec3 normal = texture2D(u_texture2, Tex_coords_displaced).xyz;
+    vec3 normal = texture2D(u_texture2, Tex_coords_displaced/5.0).xyz;
     mat3 TBN = mat3(normalize(v_tangentW), normalize(v_bitangentW), normalize(v_normalW));
     vec3 normalW = normalize(TBN * (2.0 * normal - 1.0));
     vec3 lightDirection = normalize(v_posW - u_lightPos);
@@ -50,14 +52,19 @@ void main()
 	//Fresnel
     float base = 1.0 - abs(dot(normalW, toEye));
     float Fresnel = pow(base, u_fresnelPower);
-    //vec4 rockColor = texture2D (u_texture0, v_uv);
-	vec4 rockColor = texture2D (u_texture0, Tex_coords_displaced);
-    //vec4 blendRockWater = mix(rockColor, colorWater, Fresnel);
-    vec4 blendRockWater = vec4((rockColor * colorWater).xyz, colorWater.a);
+    vec4 rockColor = texture2D (u_texture0, v_uv);
+    vec4 rockColor1 = texture2D (u_texture0, Tex_coords_displaced);
+    vec4 blendRockWater = mix(rockColor, colorWater, Fresnel);
+    //vec4 blendRockWater = vec4((rockColor * colorWater).xyz, colorWater.a);
+    
+    //Height Map
+    float heightMap = texture2D(u_texture3, v_uv/5.0).r/* + 0.5*/;
+    //vec4 vheightMap = texture2D(u_texture3, v_uv/5.0);
+    vec4 heightMapColor = vec4((blendRockWater.rgb * heightMap + vec3(0.43, 0.73, 0.96)*(1.0-heightMap)),blendRockWater.a);
     
 	//final
 	//gl_FragColor = vec4(v_normalW, 1.0);//colorWater;
 	//gl_FragColor = blendRockWater;
-	gl_FragColor = rockColor;
+	gl_FragColor = heightMapColor;
 	//gl_FragColor = vec4(diffuse,diffuse,diffuse, 1.0);
 }
